1d65dfb176b219cd1caf26eec9c135215643ba4a
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2021-12-08 16:13:31 +0100

    Add time calibration and extensions to gethisto

---
+++ Cosmic_rays/calibrate_time.py
@@ -0,0 +1,70 @@
+"""Calibrate time."""
+
+import argparse
+from typing import List
+from typing import Tuple
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+
+def lin_fit(x: List, y: List) -> Tuple:
+    X = np.array(x).reshape(len(x), 1)
+    y = np.array(y).reshape(len(y), 1)
+
+    reg = LinearRegression().fit(X, y)
+
+    return reg.coef_[0][0], reg.intercept_[0]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get coefficients of linear regression for time calibration")
+    parser.add_argument("--channel", type=str, help="Channel values of peaks") 
+    parser.add_argument("--time", type=str, help="Reference time of peaks") 
+    parser.add_argument("--channel_sigma", type=str, help="Channel sigma values of peaks") 
+    args = parser.parse_args()
+
+    channel_values = [float(c) for c in args.channel.split(",")]
+    time_values = [float(e) for e in args.time.split(",")]
+
+    if args.channel_sigma:
+        channel_sigma_values = [float(c) for c in args.channel_sigma.split(",")]
+    else:
+        channel_sigma_values = None
+
+    x = channel_values
+    y = time_values
+    m, b = lin_fit(x, y)
+
+    # get R^2
+    y_fit = [m * x_i for x_i in x]
+    r2 = r2_score(y, y_fit)
+
+    print(f"Slope = {m}")
+    print(f"Intercept = {b}")
+    print(f"R^2 = {r2}")
+
+    # plt.figure(figsize=(12, 8))
+    if channel_sigma_values:
+        plt.errorbar(x, y, xerr=channel_sigma_values, fmt='.', c='b')
+    else:
+        plt.plot(x, y, 'b.', markersize=10)
+
+    x_interp = np.linspace(x[0] - 10, x[-1] + 10, 100)
+    y_interp = [m * x_i + b for x_i in x_interp]
+
+    plt.rcParams.update({'font.size': 17})
+
+    label = "m = {:.2f}, b = {:.2f}".format(m, b)
+    plt.plot(x_interp, y_interp, c='k', label=label)
+    plt.grid()
+    plt.legend()
+    plt.xticks(fontsize=17)
+    plt.yticks(fontsize=17)
+    plt.xlabel("Channel", fontsize=17)
+    plt.ylabel("Time (ns)", fontsize=17)
+    plt.title("$R^2 = {:.2f}$".format(r2))
+    # plt.tight_layout()
+    plt.show()
--- Cosmic_rays/data_analysis_examples/gethisto.C
+++ Cosmic_rays/data_analysis_examples/gethisto.C
@@ -84,7 +84,16 @@ TGraph *getSignal(const char *name_file, int nSamples=250, short chan=0, int nrE
 }
 
 
-TH1D* getHistoWithFilterFromChannel(const char *name_file, int numBins, double minX, double maxX, short chan1, short chan2, double thres1, double thres2) {
+TH1D* getHistoWithFilterFromChannel(
+	const char *name_file,
+	int numBins,
+	double minX,
+	double maxX,
+	short chan1,
+	short chan2,
+	double thres1,
+	double thres2
+) {
 	// variables
 	slimport_data_t indata1, indata2;
 	TFile *infile = new TFile(name_file);
@@ -101,6 +110,69 @@ TH1D* getHistoWithFilterFromChannel(const char *name_file, int numBins, double m
 		if (indata2.qlong>thres1 && indata2.qlong<thres2) {
 			h_spectrum->Fill(indata1.qlong);
 		}
+	}
+	// return
+	return h_spectrum;
+}
+
+
+TH1D* getHistoWithFilterFromChannelWithCalibration(
+	const char *name_file,
+	const char *histo_name,
+	int numBins,
+	double minX,
+	double maxX,
+	short chan1,
+	short chan2,
+	double thres1,
+	double thres2,
+	double mCalibCoef,
+	double bCalibCoef
+) {
+	// variables
+	slimport_data_t indata1, indata2;
+	TFile *infile = new TFile(name_file);
+	TTree *intree = (TTree*)infile->Get("acq_tree_0");
+	TBranch *inbranch1 = intree->GetBranch(Form("acq_ch%d",chan1));
+	TBranch *inbranch2 = intree->GetBranch(Form("acq_ch%d",chan2));
+	inbranch1->SetAddress(&indata1.timetag);
+	inbranch2->SetAddress(&indata2.timetag);
+	TH1D *h_spectrum = new TH1D("h_spectrum",histo_name,numBins,minX,maxX);
+	// histogram filling
+	for (int i=0; i<inbranch1->GetEntries(); i++) {
+		inbranch1->GetEntry(i);
+		inbranch2->GetEntry(i);
+		if (indata2.qlong>thres1 && indata2.qlong<thres2) {
+			double calibratedValue = mCalibCoef * (double)indata1.qlong + bCalibCoef;
+			h_spectrum->Fill(calibratedValue);
+		}
+	}
+	// return
+	return h_spectrum;
+}
+
+
+TH1D* getHistoForChannelFromTreeWithCalibration(
+	const char *name_file,
+	short chan,
+	int numBins,
+	double minX,
+	double maxX,
+	double mCalibCoef,
+	double bCalibCoef
+) {
+	// variables
+	slimport_data_t indata;
+	TFile *infile = new TFile(name_file);
+	TTree *intree = (TTree*)infile->Get("acq_tree_0");
+	TBranch *inbranch = intree->GetBranch(Form("acq_ch%d",chan));
+	inbranch->SetAddress(&indata.timetag);
+	TH1D *h_spectrum = new TH1D("h_spectrum","Total spectrum",numBins,minX,maxX);
+	// histogram filling
+	for (int i=0; i<inbranch->GetEntries(); i++) {
+		inbranch->GetEntry(i);
+		double calibratedValue = mCalibCoef * (double)indata.qlong + bCalibCoef;
+		h_spectrum->Fill(calibratedValue);
 	}
 	// return
 	return h_spectrum;
